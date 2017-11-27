d383ca2b34d99dd82e5e67fdcd479533195e8a67
Florian <Florian@192.168.1.122>
2017-11-27 01:19:13 +0100

    Adding tests

---
+++ WebServer/src/test/java/pixels/webserver/hello/RandomApplicationTest.java
@@ -0,0 +1,23 @@
+package pixels.webserver.hello;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RandomApplicationTest {
+	
+@Test
+public void testFuntction()
+{
+	int result = 1+1;
+	assertEquals(2, result);
+	}
+	
+@Test
+public void WorkingTest()
+{
+	int result = 3;
+	assertEquals(3,result);
+	}
+
+}

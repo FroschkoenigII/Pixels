8cebead1c2ffc72b1e8288042866b8ecf3721d79
langenf <florian.langen@msg-gillardon.de>
2017-12-03 19:58:06 +0100

    Addinge some tests

--- WebServer/src/test/java/pixels/webserver/hello/RandomApplicationTest.java
+++ WebServer/src/test/java/pixels/webserver/hello/RandomApplicationTest.java
@@ -7,14 +7,28 @@ import org.junit.Test;
 public class RandomApplicationTest {
 	
 @Test
-public void testFuntction()
+public void testFuntctionWorking()
 {
 	int result = 1+1;
 	assertEquals(2, result);
 	}
 	
 @Test
-public void WorkingTest()
+public void testFuntctionNotWorking()
+{
+	int result = 3;
+	assertEquals(2,result);
+	}
+
+@Test
+public void testFuntctionWorking2()
+{
+	int result = 3;
+	assertEquals(3,result);
+	}
+
+@Test
+public void testFuntctionNotWorking2()
 {
 	int result = 3;
 	assertEquals(2,result);
---
+++ WebServer/src/test/java/pixels/webserver/hello/WorkingTest.java
@@ -0,0 +1,33 @@
+package pixels.webserver.hello;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WorkingTest {
+
+	@Test
+	public void WorkingTestFunction1() {
+		int result = 3;
+		assertEquals(3, result);
+	}
+
+	@Test
+	public void WorkingTestFunction2() {
+		int result = 3;
+		assertEquals(3, result);
+	}
+
+	@Test
+	public void WorkingTestFunction3() {
+		int result = 3;
+		assertEquals(3, result);
+	}
+
+	@Test
+	public void WorkingTestFunction4() {
+		int result = 3;
+		assertEquals(3, result);
+	}
+
+}

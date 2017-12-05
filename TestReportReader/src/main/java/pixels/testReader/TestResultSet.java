1b436d6e0989fb6c90e5f7e3ed554a69f86036d7
langenf <florian.langen@msg-gillardon.de>
2017-12-05 06:01:55 +0100

    Parsing

--- TestReportReader/src/main/java/pixels/testReader/TestResultSet.java
+++ TestReportReader/src/main/java/pixels/testReader/TestResultSet.java
@@ -4,12 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestResultSet {
+	
+	private String testclassPath;
 
+	private String testclassName;
+	
 	private List<String> messageLines;
 	
-	private String errormessage;
+	private List<String> errormessages;
 	
 	private String originalMessage;
+	
+	public String getTestclassPath() {
+		return testclassPath;
+	}
+
+	public void setTestclassPath(String testclassPath) {
+		this.testclassPath = testclassPath;
+	}
 
 	public List<String> getMessageLines() {
 		if(messageLines == null)
@@ -17,16 +29,26 @@ public class TestResultSet {
 		return messageLines;
 	}
 
+	public String getTestclassName() {
+		return testclassName;
+	}
+
+	public void setTestclassName(String testclassName) {
+		this.testclassName = testclassName;
+	}
+
 	public void setMessageLines(List<String> messageLines) {
 		this.messageLines = messageLines;
 	}
 
-	public String getErrormessage() {
-		return errormessage;
+	public List<String> getErrormessage() {
+		if(errormessages == null)
+			errormessages = new ArrayList<String>();
+		return errormessages;
 	}
 
-	public void setErrormessage(String errormessage) {
-		this.errormessage = errormessage;
+	public void setErrormessage(List<String> errormessage) {
+		this.errormessages = errormessage;
 	}
 
 	public String getOriginalMessage() {
--- TestReportReader/src/main/java/pixels/testReader/reader/testResultReaderImpl.java
+++ TestReportReader/src/main/java/pixels/testReader/reader/testResultReaderImpl.java
@@ -16,26 +16,17 @@ import pixels.testReader.TestResultSet;
 public class testResultReaderImpl implements testResultReader {
 
 	public List<TestResultSet> readTestresults(String path) throws testResultReaderException {
-		
+
 		List<File> testfiles = new ArrayList<File>();
 		List<TestResultSet> testSets = new ArrayList<TestResultSet>();
-		try (Stream<Path> paths = Files.walk(Paths.get("C:\\Users\\langenf\\Desktop\\TestFolder"))) {
-			testfiles =paths
-		        .filter(Files::isRegularFile)
-		        .map(Path::toFile)
-		        .collect(Collectors.toList());   	
-		}catch(IOException e)
-		{
-			throw new testResultReaderException(e);
-		}
-		
-		for(File f : testfiles)
-		{
-			if (f.getName().endsWith(".txt"))
-			{
+		testfiles.addAll(collectFilesFromPath("C:\\Users\\langenf\\Desktop\\TestFolder"));
+
+		for (File f : testfiles) {
+			if (f.getName().endsWith(".txt")) {
 				System.out.println(f.getName());
 				TestResultSet trs = new TestResultSet();
 				try {
+					trs.setTestclassPath(f.getName());
 					trs.getMessageLines().addAll(Files.readAllLines(f.toPath()));
 					testSets.add(trs);
 				} catch (IOException e) {
@@ -43,16 +34,82 @@ public class testResultReaderImpl implements testResultReader {
 				}
 			}
 		}
-		
-		for(TestResultSet ts : testSets)
-		{
-			for(String line : ts.getMessageLines())
-			{
-				System.out.println(line);
+		for (TestResultSet ts : testSets) {
+			try {
+				parseToObject(ts);
+			} catch (IndexOutOfBoundsException e) {
+				System.out.println("Error while parsing " + ts.getTestclassPath() + e.getMessage());
 			}
 		}
-		
+
 		return null;
+	}
+
+	private void parseToObject(TestResultSet ts) {
+
+		String testNameLine = ts.getMessageLines().get(1);
+		ts.setTestclassName(testNameLine.substring(9));
+		String testErrorLine = ts.getMessageLines().get(3);
+
+		String testErrors = testErrorLine.substring(testErrorLine.indexOf("Failures: ") + 10,
+				testErrorLine.indexOf(", Errors"));
+		int errorCount = Integer.valueOf(testErrors);
+
+		System.out.println(ts.getTestclassName() + " has " + errorCount + " errors!");
+
+		if (errorCount != 0) {
+
+			int currentLine = 4;
+			for (int i = 0; i < errorCount; i++) {
+				String errormessage = "";
+				while (currentLine + 1 < ts.getMessageLines().size()
+						&& !ts.getMessageLines().get(currentLine).equals("")) {
+					errormessage = errormessage.concat(ts.getMessageLines().get(currentLine));
+					errormessage = errormessage.concat("\n");
+					currentLine++;
+				}
+				currentLine++;
+				ts.getErrormessage().add(errormessage);
+				//System.out.println("Errolar msg "+i +": " + errormessage);
+				
+				/*
+				 *
+{
+    "fields": {
+       "project":
+       { 
+          "id": "10701"
+       },
+       "summary": "TestIssue.",
+       "description": "TestIssue",
+       "issuetype": {
+          "id": "10601"
+       },
+       "components": [
+       	{
+       	"id": "10934"
+        }
+       ]
+   }
+}
+				 * 
+				 * 
+				 * 
+				 * 
+				 */
+			}
+		}
+
+	}
+
+	private List<File> collectFilesFromPath(String path) throws testResultReaderException {
+		List<File> testfiles = new ArrayList<File>();
+		try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+			testfiles = paths.filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList());
+		} catch (IOException e) {
+			throw new testResultReaderException(e);
+		}
+		return testfiles;
 	}
 
 }

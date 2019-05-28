fd51c27f253b5cfa366fd97b60d0e6fba6f51ced
Florian <florian.langen@googlemail.com>
2019-05-29 00:16:21 +0200

    Testscipts

--- executeScript.sh
+++ executeScript.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo SCRIPT STARTING
 pwd
+
 whoami
-ls -l
+
+java -jar /PixelsFreshStart/target/PixelsFreshStart-0.1.0.jar --port=8081 & 
+
+MyPID=$!                        # You sign it's PID
+echo $MyPID                     # You print to terminal
+
+echo "echo $MyPID" > stopScript.sh

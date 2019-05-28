c7bd210cd4b7778652df27e0d7859758047ac8b6
Florian <florian.langen@googlemail.com>
2019-05-29 01:45:19 +0200

    dds

--- executeScript.sh
+++ executeScript.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-
-
 echo SCRIPT STARTING
+
 pwd
 
 whoami
 
+BUILD_ID=dontKillMe
+
 java -Dserver.port=8081 -jar PixelsFreshStart/target/PixelsFreshStart-0.1.0.jar &
 
-ps -aux
+sleep 10
+
+ps -a

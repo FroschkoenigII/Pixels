8277ae6e572feea4305a74f5824b39b9fe0b3393
Florian <florian.langen@googlemail.com>
2019-05-29 00:34:42 +0200

    xecute

--- executeScript.sh
+++ executeScript.sh
@@ -4,11 +4,4 @@ echo SCRIPT STARTING
 pwd
 
 whoami
-java -Dserver.port=8081 -jar PixelsFreshStart/target/PixelsFreshStart-0.1.0.jar &
-
-MyPID=$!                        # You sign it's PID
-echo $MyPID                     # You print to terminal
-
-disown
-
-echo "kill $MyPID" > stopScript.sh
+java -Dserver.port=8081 -jar PixelsFreshStart/target/PixelsFreshStart-0.1.0.jar

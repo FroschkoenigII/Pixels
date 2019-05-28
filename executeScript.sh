ec24c13547f612f93bae1a804006ab186bb372c1
Florian <florian.langen@googlemail.com>
2019-05-29 00:27:08 +0200

    add disown

--- executeScript.sh
+++ executeScript.sh
@@ -9,4 +9,6 @@ java -Dserver.port=8081 -jar PixelsFreshStart/target/PixelsFreshStart-0.1.0.jar 
 MyPID=$!                        # You sign it's PID
 echo $MyPID                     # You print to terminal
 
+disown
+
 echo "kill $MyPID" > stopScript.sh

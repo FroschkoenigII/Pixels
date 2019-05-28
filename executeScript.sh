a8fb78a434adca0444a97decfb72a794ce4b5eba
Florian <florian.langen@googlemail.com>
2019-05-29 00:43:35 +0200

    exe

--- executeScript.sh
+++ executeScript.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+
+
 echo SCRIPT STARTING
 pwd
 
 whoami
-java -Dserver.port=8081 -jar PixelsFreshStart/target/PixelsFreshStart-0.1.0.jar
+
+
+java -Dserver.port=8081 -jar PixelsFreshStart/target/PixelsFreshStart-0.1.0.jar &
+
+pid=`ps aux | grep PixelsFreshStart | awk '{print $2}'`
+
+disown pid

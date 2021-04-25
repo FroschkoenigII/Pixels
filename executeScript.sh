1a973aa19c2429db607199c2cd4ab88249eb73ad
Florian <florian.langen@googlemail.com>
2021-04-25 20:25:29 +0200

    update

--- executeScript.sh
+++ executeScript.sh
@@ -1,7 +1,7 @@
 echo Killing old Run 
 pid=`ps aux | grep PixelsFreshStart | awk '{print $2}'`
 echo Killing $pid
-//kill -9 $pid
+#kill -9 $pid
 echo Starting new Run
-BUILD_ID=dontKillMe
+JENKINS_NODE_COOKIE=dontKillMe
 java -jar PixelsFreshStart/target/PixelsFreshStart-0.1.0.jar &
